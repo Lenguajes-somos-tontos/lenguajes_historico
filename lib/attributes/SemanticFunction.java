0f5ce933ed748432d5bf5146b28f0d19706d69bc
Iortxi <jortuaito11@gmail.com>
2024-04-15 17:02:16 +0200

    Mejora de Instruccion y Asignación con cosas residuales

--- lib/attributes/SemanticFunction.java
+++ lib/attributes/SemanticFunction.java
@@ -28,6 +28,7 @@ public class SemanticFunction {
 		}
     }
 
+
     public Symbol Tipo_variable(Token t, Symbol result) {
         switch (t.image.toLowerCase()) {
 			case "boolean":
@@ -43,12 +44,14 @@ public class SemanticFunction {
 		return result;
     }
 
+
     public Symbol Tipo_array(Token indMin, Token indMax, Symbol tipo_base) {
 		int indMin_i = Integer.parseInt(indMin.image);
 		int indMax_i = Integer.parseInt(indMax.image);
 		Symbol result = new SymbolArray("", indMin_i, indMax_i, tipo_base.type);
 		return result;
     }
+
 
     public void Primario_ID(Token t, Par tipo, Symbol simbolo, SymbolTable st) {
     	try {
@@ -91,123 +94,51 @@ public class SemanticFunction {
 		}
     }
 
-/*
-    public Par Factor(Par tipo, Par tipo2, boolean match2) {     
-		if (match2 && tipo2.primero != Symbol.Types.BOOL) {
-			tipo.primero = Symbol.Types.UNDEFINED;
-		}
-		else if (match2) {
-			tipo.primero = tipo2.primero;	// tipo.primero = BOOL
-			tipo.segundo = false;
-		}
-		return tipo;
-	}
 
-
-    public Par Termino(Par tipo, Par tipo2, boolean match2) {     
-		if (match2 && (tipo.primero != Symbol.Types.INT || tipo2.primero != Symbol.Types.INT)) { 
-			tipo.primero = Symbol.Types.UNDEFINED;
-		}
-		else if (match2) {
-			tipo.segundo = false;
-		}
-		return tipo;
-	}
-
-    public Par Expresion_simple(Par tipo, Par tipo2, boolean match1, boolean match2) {     
-		if ((match1 && tipo.primero != Symbol.Types.INT) || (match2 && (tipo.primero != Symbol.Types.INT || tipo2.primero != Symbol.Types.INT))) {
-			tipo.primero = Symbol.Types.UNDEFINED;
-		}
-		else if (match1 || match2) {
-			tipo.segundo = false;
-		}
-		return tipo;
-	}
-
-    public Par Relacion(Par tipo, Par tipo2, Token op, boolean match2) {     
-		if (match2) {
-			if (tipo.primero != tipo2.primero) {
-				tipo.primero = Symbol.Types.UNDEFINED; 
-			}
-			tipo.segundo = false;
-		}
-		if (tipo.primero != Symbol.Types.UNDEFINED) {
-			if (match2 && (op.image.equals("=") || op.image.equals("/="))) {
-				if (tipo.primero == Symbol.Types.STRING || tipo.primero == Symbol.Types.ARRAY) {
-					tipo.primero = Symbol.Types.UNDEFINED;
-				}
-				else {
-					tipo.primero = Symbol.Types.BOOL;
-				}
-			}
-			else if (match2 && (!op.image.equals("=") || !op.image.equals("/="))) {
-				if (tipo.primero != Symbol.Types.INT || tipo.primero == Symbol.Types.CHAR) {
-					tipo.primero = Symbol.Types.UNDEFINED;
-				}
-				else {
-					tipo.primero = Symbol.Types.BOOL;
-				}
-			}
-		}
-		return tipo;
-	}
-/*
-    public Par Expresion(Par tipo, Par tipo2, boolean match2) {     
-		if (match2 && (tipo.primero != Symbol.Types.BOOL || tipo2.primero != Symbol.Types.BOOL)) {
-			tipo.primero = Symbol.Types.UNDEFINED;
-		}
-		else if (match2) {
-			tipo.segundo = false;
-		}
-		System.out.println("Expresion: " + tipo.primero + " " + tipo.segundo);
-		return tipo;
-	}
-*/
-	public void Asignacion(Token id, Par tipo_asignacion, Symbol simbolo_asignacion, SymbolTable st) {
+	public void Asignacion(String id, Par tipo_asignacion, SymbolTable st) {
 		try {
-			simbolo_asignacion = st.getSymbol(id.image);
-
+			Symbol simbolo_asignacion = st.getSymbol(id);
 			Symbol.Types tipo_id = simbolo_asignacion.type;
-			if (!((tipo_id == tipo_asignacion.primero) &&
-				((tipo_id != Symbol.Types.FUNCTION) || (tipo_id != Symbol.Types.PROCEDURE) || tipo_id != Symbol.Types.ARRAY))) {
+
+			if (tipo_id != tipo_asignacion.primero || tipo_id == Symbol.Types.FUNCTION
+				|| tipo_id == Symbol.Types.PROCEDURE || tipo_id == Symbol.Types.ARRAY || tipo_id == Symbol.Types.STRING) {
 				System.out.println("ERROR: Se esperaba un tipo " + tipo_id);
 			}
 		}
 		catch (SymbolNotFoundException s) {
-			System.out.println("ERROR: El simbolo " + id.image + " no está definido");
+			System.out.println("ERROR: El simbolo " + id + " no está definido");
 		}
 	}
 
-	public void Instruccion(Token id, boolean llamada_funcion_simple, SymbolTable st) {
-		// Si se entra al if, es que se ha llamado a un procedimiento/función
-		if (llamada_funcion_simple) {
-			try {
-				Symbol simbolo_proc_func = st.getSymbol(id.image);
 
-				if ((simbolo_proc_func.type != Symbol.Types.FUNCTION) && (simbolo_proc_func.type != Symbol.Types.PROCEDURE)) {
-					System.out.println("ERROR: Se esperaba un procedimiento o función");
+	public void Instruccion(String id, SymbolTable st) {
+		try {
+			Symbol simbolo_proc_func = st.getSymbol(id);
+
+			if (simbolo_proc_func.type != Symbol.Types.FUNCTION && simbolo_proc_func.type != Symbol.Types.PROCEDURE) {
+				System.out.println("ERROR: Se esperaba un procedimiento o función");
+			}
+			else {
+				// El ID que se ha escrito es un procedimiento/función, verificar que no tiene parámetros
+				ArrayList<Symbol> lista_parametros;
+				if (simbolo_proc_func.type == Symbol.Types.FUNCTION) {
+					SymbolFunction s = (SymbolFunction) simbolo_proc_func;
+					lista_parametros = s.parList;
 				}
 				else {
-					// El ID que se ha escrito es un procedimiento/función, verificar que no tiene parámetros
-					if (simbolo_proc_func.type == Symbol.Types.FUNCTION) {
-						SymbolFunction s = (SymbolFunction) simbolo_proc_func;
-						if (!s.parList.isEmpty()) {
-							System.out.println("ERROR: Los parámetros de la función " + id.image + " no son correctos");
-						}
-					}
-					else {
-						SymbolProcedure s = (SymbolProcedure) simbolo_proc_func;
-						if (!s.parList.isEmpty()) {
-							System.out.println("ERROR: Los parámetros del procedimiento " + id.image + " no son correctos");
-						}
-					}
+					SymbolProcedure s = (SymbolProcedure) simbolo_proc_func;
+					lista_parametros = s.parList;
+				}
+				if (!lista_parametros.isEmpty()) {
+					System.out.println("ERROR: Los parámetros de la llamada a " + id + " no son correctos");
 				}
 			}
-			catch (SymbolNotFoundException s) {
-				System.out.println("ERROR: El procedimiento o función " + id.image + " no está definido");
-			}
+		}
+		catch (SymbolNotFoundException s) {
+			System.out.println("ERROR: El procedimiento o función " + id + " no está definido");
 		}
 	}
+
 
 	public void Instr_funcion_vector_3(String id, Par tipo_primera_expresion, Par tipo_asignacion, SymbolTable st) {
 		try {
@@ -233,6 +164,7 @@ public class SemanticFunction {
 		}
 	}
 
+
 	public void verificar_parametro_procedimiento(SymbolProcedure p, Par par, int iterador) {
 		if (p.parList.get(iterador).type != par.primero) {
 			System.out.println("ERROR: Se esperaba un tipo " + p.parList.get(iterador).type + " en la llamada a " + p.name); // Y la línea
@@ -241,6 +173,7 @@ public class SemanticFunction {
 			System.out.println("ERROR: El tipo " + p.parList.get(iterador).type + " en la llamada a " + p.name + " debe ser una variable"); // Y la línea
 		}
 	}
+
 
 	public void verificar_parametro_funcion(SymbolFunction p, Par par, int iterador) {
 		if (p.parList.get(iterador).type != par.primero) {
